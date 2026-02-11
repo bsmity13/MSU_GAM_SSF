7173fb1a6e71bfca9450c93d299e3986312144da
Brian Smith <bsmity13@gmail.com>
2026-02-10 21:28:37 -0800

    Minor updates to GAM lecture/walkthrough

--- 04_fit_GAMs.R
+++ 04_fit_GAMs.R
@@ -3,12 +3,23 @@
 # We will fill this in during the workshop and then update this script.
 
 # Load packages ----
+library(dplyr)
+library(mgcv)
+library(lubridate)
+library(gratia)
 
 # Load data ----
 # Simulated data
 
-# Real data
+# Real data (base R dataset)
+# Data are from New York City in 1973
+data("airquality")
+
+airquality <- airquality %>% 
+  mutate(date = ymd(paste("1973", Month, Day, sep = "-")),
+         doy = yday(date))
 
 # Fit GAM to simulated data ----
 
 # Fit GAM to real data ----
+# Ozone depends on day of year and temperature
